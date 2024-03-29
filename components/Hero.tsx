4bdb2dcdd35b98bb8dcc077fb084690ac43a2c17
Ali Zilani <ma21chb0b43@student.nitw.ac.in>
2024-03-29 12:04:42 +0530

    in hero section added category

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import category from '@/data/category'
 
 function Hero() {
   return (
@@ -12,13 +13,23 @@ function Hero() {
                 <h2 className='text-[20px]'>Your Amazing City</h2>
                 <div className='mt-5 z-10 flex gap-2 item-center justify-center'>
                     <input type='text' placeholder='Seacrh Anything' 
-                    className=' bg-white p-3 z-10 border-[1px] rounded-full px-5 w-[36%] shadow-lg outline-red-300'/>
-                    <button className='bg-red-600 rounded-full p-3 shadow-md z-10 cursor-pointer hover:scale-105 transition-all'> 
+                    className=' bg-white p-3 z-10 border-[1px] rounded-full px-5 w-[36%] shadow-lg outline-red-400'/>
+                    <button className='bg-red-500 rounded-full p-3 shadow-md z-10 cursor-pointer hover:scale-105 transition-all text-white'> 
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
                         strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" 
                         d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" /></svg>
-                        
                     </button>
+                </div>
+            </div>
+            <div className='mt-5 flex flex-col items-center justify-center'>
+                <h2>Or Browse the category</h2>
+                <div className='grid grid-cols-3 md:grid-cols-7 w-[50%] justify-center mt-3' >
+                    {category.map((item,index)=>(
+                        <div key={item.id} 
+                            className='border-[1px] w-[60px] p-4 bg-white rounded-full z-10 cursor-pointer hover:border-red-500 hover:scale-110 transition-all' >
+                            <Image src={item.icon} alt={item.name} width={30} height={30} />
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
