223322dcbc1c2cfb44ac3cc17a908b869a29f1f0
Ali Zilani <ma21chb0b43@student.nitw.ac.in>
2024-03-29 09:58:35 +0530

    added custom font

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
-import { url } from 'inspector'
+
 
 function Header() {
   return (
     <div className='flex justify-between px-5 p-3 shadow-sm'>
       <div className='flex gap-3 items-cneter'>
       <Image src='/logo_img.png' alt='logo' width={50} height={50} />
-      <h2 className='text-[25px] text-red-500 font-semibold tracking-widest'>Discover</h2>
+      <h2 className='text-[25px] text-red-500 font-bold tracking-widest py-2'>DISCOVER</h2>
       </div>
       <ul className='flex gap-8 items-center'>
         <li className='text-[18px] hover:text-red-500 cursor-pointer '>Home</li>
